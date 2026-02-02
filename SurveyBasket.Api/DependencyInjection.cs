6627684bbfb90f25fa5502e758a5234dab7a5987
Mohamed AbdElnasser <abdelnasserm18@gmail.com>
2026-02-02 16:13:15 +0200

    MoveDependencies

--- SurveyBasket.Api/DependencyInjection.cs
+++ SurveyBasket.Api/DependencyInjection.cs
@@ -4,38 +4,41 @@
     {
         public static IServiceCollection AddDependencies(this IServiceCollection services)
         {
-            // Register API related services here in the future
-
-
-            // Add services to the container.
-
-            services.AddScoped<IPollService, PollService>();
-
-            // Add Mapster configurations
-            // ????? ????????? ???? ???? ?????? ?????
-
-            var mappingConfig = TypeAdapterConfig.GlobalSettings;
-            mappingConfig.Scan(Assembly.GetExecutingAssembly());
-
-            services.AddSingleton<IMapper>(new Mapper(mappingConfig));
-            ////////
-            ///
-            // Add FluentValidation validators
-            //services.AddScoped<IValidator<CreatePollRequest>, CreatePollRequestValidator>();
-            services
-              .AddFluentValidationAutoValidation()
-              .AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-
-
+            
+            services.AddServicesToContainer();
+            services.AddMappsterConf();
             services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             services.AddOpenApi();
+            services.AddFluentValidation();
 
 
             return services;
         }
 
-        
+        public static IServiceCollection AddServicesToContainer(this IServiceCollection services)
+        {
+            // Add services to the container.
+            services.AddScoped<IPollService, PollService>();
 
+            return services;
+        }
+
+        public static IServiceCollection AddMappsterConf(this IServiceCollection services)
+        {
+            var mappingConfig = TypeAdapterConfig.GlobalSettings;
+            mappingConfig.Scan(Assembly.GetExecutingAssembly());
+            services.AddSingleton<IMapper>(new Mapper(mappingConfig));            
+
+            return services;
+        }
+
+        public static IServiceCollection AddFluentValidation(this IServiceCollection services)
+        {
+            services
+              .AddFluentValidationAutoValidation()
+              .AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
     }
 }
